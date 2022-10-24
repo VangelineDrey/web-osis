c6bb01f102c063c71f1eec8dc9625dacbdb45d81
vinsen <vinsensutanto@gmail.com>
2022-10-24 10:29:48 +0700

    optimasi keanggotaan dan anggota

--- bersamabangunosis/keanggotaan.php
+++ bersamabangunosis/keanggotaan.php
@@ -1,5 +1,9 @@
 <?php
-
+include 'admin/functions.php';
+if(isset($_GET['divisi'])){
+    $divisi = $_GET['divisi'];
+    $datas = query("SELECT * FROM anggota WHERE divisi = '$divisi'");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Classroom | SMK</title>
+    <title>Keanggotaan | SMK</title>
     <style>
             /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}}
         </style>
@@ -30,78 +34,77 @@
                 left:0px;
                 z-index:-1;
             }
-        .container {  
-            display: grid;  
-            grid-gap: 5px;  
-            row-gap: 4em;
-            grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-            grid-template-rows: repeat(2, 300px); 
-            justify-items: center; 
+                .container {  
+                    display: grid;  
+                    grid-gap: 5px;  
+                    row-gap: 4em;
+                    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+                    grid-template-rows: repeat(2, 300px); 
+                    justify-items: center; 
+                }
+                div{
+                    height: 100%;
+                }
+                .container > div > img {  
+                    width: 100%;  
+                    height: 100%;  
+                    object-fit: cover;  
+                }
+                img{
+                    max-width: 200px;
+                    max-height: 300px;
+                }
+                p{
+                    text-align: center;
+                    font-size:1em;
+                }
+                a{
+                    color:white;
+                    text-decoration:none;
+                }
+                a:hover{
+                    text-decoration:bold;
+                }
+                @media (max-width: 768px) {
+                    body { 
+        background: url(assets/images/bg-classroom.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
         }
-        div{
-            height: 100%;
-        }
-        .container > div > img {  
-            width: 100%;  
-            height: 100%;  
-            object-fit: cover;  
-        }
-        img{
-            max-width: 200px;
-            max-height: 300px;
-        }
-        p{
-            text-align: center;
-            font-size:1em;
-        }
-        a{
-            color:white;
-            text-decoration:none;
-        }
-        a:hover{
-            text-decoration:bold;
-        }
-        @media (max-width: 768px) {
-            body { 
-  background: url(../img/bg-classroom.jpg) no-repeat center center fixed; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-        }
+                }
     </style>
 </head>
 <body>
     <div class="overlay"></div>
                 <div class="fixed top-0 right-0 px-6 py-4 sm:block">
-                        <a href="/" ><i class="fa fa-arrow-left w3-hover-opacity"></i>&nbsp;Back</a>
+                        <a href="index.php#keanggotaan" ><i class="fa fa-arrow-left w3-hover-opacity"></i>&nbsp;Back</a>
                 </div>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="justify-center pt-8 sm:justify-start sm:pt-0" style="text-align:center;margin-bottom:10px;">
                     <i style="font-size:3em;margin:auto;">
-                    @foreach($kelas as $key=>$kel)
-                        {{$kel->name}}
-                    @endforeach
+                    <?php 
+                        echo $divisi;
+                    ?>
                     </i>
                 </div>
 
-    @if(count($students)>0)
         <div class="container">
-        @foreach($students as $key=>$student)
+        <?php 
+        foreach($datas as $data){?>
             <div style="background-color:black; max-width:200px;max-height:300px">
-            @if($student->image !="")
-                <img src="{{ asset('img/people') }}/{{ $student->image }}" onclick="window.location.href = '../detail/{{$student->class_id}}/{{$student->id}}'"/>
-            @else
-                <img src="{{ asset('img') }}/noprofile.jpg" onclick="window.location.href = '../detail/{{$student->class_id}}/{{$student->id}}'"/>
-            @endif
-            <a href="../detail/{{$student->class_id}}/{{$student->id}}">
-            <p style="margin-top:3px;">{{ $student->name }} | {{ $student->nickname }}</p>
+            <?php if($data['image']!=""){?>
+                <img src="assets/images/people/<?php echo $data['image'];?>" onclick="window.location.href = '../anggota/<?php echo $data['id']; ?>'"/>
+            <?php } else {?>
+                <img src="assets/images/noprofile.jpg" onclick="window.location.href = '../anggota/<?php echo $data['id']; ?>'"/>
+            <?php } ?>
+            <a href="anggota.php?id=<?php echo $data['id']; ?>">
+            <p style="margin-top:3px;"><?php echo $data['name'] ?> | <?php echo $data['akhirjabatan']; ?></p>
             </a>
             </div>  
-        @endforeach
-    @endif    
+        <?php }; ?>   
         </div>
 </body>
 </html>
